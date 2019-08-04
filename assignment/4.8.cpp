2bcdaa92aa87d59a59590802d0c4f767ef12369c
shwetap1995 <patilshwetambari1995@gmail.com>
2019-08-04 20:29:06 +0530

    pattern5

---
+++ assignment/4.8.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream.h>
+#include<conio.h>
+void main() {
+clrscr();
+for (int i = 1; i <=5; i++) {
+	for (int j = 5; j >=1; j--) {
+	if(i>=j) {
+		cout<<"*";
+	    }
+	    else {
+	    cout<<" ";
+	    }
+	       }
+        cout<<"\n";
+        }
+        getch();
+}
